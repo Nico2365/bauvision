fe7f23e773f608d9b3ec5f255a88363acb98a593
Nico2365 <nico.gruendler@gmx.de>
2025-07-08 13:58:43 +0200

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS7Q0h6-Pc-kB1Dqa-WlB2LdaLnl4zaY4",
+  authDomain: "bau-vision25.firebaseapp.com",
+  projectId: "bau-vision25",
+  storageBucket: "bau-vision25.appspot.com",
+  messagingSenderId: "399020543777",
+  appId: "1:399020543777:web:7011999a6514a2c4043860",
+  measurementId: "G-4E4XP60LWY"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
